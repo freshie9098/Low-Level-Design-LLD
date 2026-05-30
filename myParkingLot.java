fafab5580e92515397d51fac364000058c2b7d1a
freshie9098 <57678075+freshie9098@users.noreply.github.com>
2026-05-30 22:45:53 +0530

    Create myParkingLot.java

---
+++ myParkingLot.java
@@ -0,0 +1,131 @@
+//VEHICLE
+
+public enum VehicleType{
+    CAR,
+    BIKE,
+    TRUCK
+}
+
+public abstract class Vehicle{
+    private String vehicleId;
+    private vehicleType vehicleType;
+    
+    public Vehicle(String vehicleId,vehicleType vehicleType){
+        this.vehicleId = vehicleId;
+        this.vehicleType = vehicleType;
+    }   
+    
+    public String getVehicleId(){
+        return vehicleId;
+    }
+    public vehicleType getVehicleType(){
+        return vehicleType;
+    }
+    
+    public abstract int getSpaceRequired();
+} 
+
+public class Car extends vehicle{
+    public Car(String vehicleId){
+        super(vehicleId,vehicleType.CAR);
+    }
+    public int getSpaceRequired(){
+        return 1;
+    }
+}
+//spot
+public enum SpotType{
+    CAR,TRUCK,BIKE
+}
+
+public class ParkingSpot{
+   
+    private final String spotId;
+    SpotType spotType;
+    private boolean isAvailable = true;
+    private Vehicle parkedVehicle;
+    
+    public ParkingSpot(String spotId,SpotType spotType){
+        this.spotId = spotId;
+        this.spotType = spotType;
+    }
+    
+    public String getSpotId(){
+        return spotId;
+    }
+    
+    public SpotType getspotType(){
+        return spotType;
+    }
+    
+    public Vehicle getParkedVehicle(){
+        return parkedVehicle;
+    }
+    
+    public synchronized boolean getisAvailable(){
+        return isAvailable;
+    }
+    
+    public synchronized void parkVehicle(Vehicle vehicle){
+        if(!isAvailable || !canFitVehicle)return;
+        this.parkedVehicle = vehicle;
+        this.isAvailable = false;
+    }
+    public synchronized void unParkVehicle(){
+        this.parkedVehicle = null;
+        this.isAvailable = true;
+    }
+    
+    public boolean canFitVehicle(Vehicle vehicle){
+        return switch (vehicle.getVehicleType()) {
+            case CAR->spotType == SpotType.CAR;
+            case BIKE->spotType == SpotType.BIKE;
+            case TRUCK->spotType == SpotType.TRUCK;
+        }
+    }
+    
+}
+
+
+//FLOOR
+public class Floor{
+    private  final Map<String,ParkingSpot>spots;
+    private String floorId;
+    
+    
+    //
+    public Floor(String floorId){
+        this.floorId = floorId;
+        this.spots = new ConcurrentHashMap<>();
+    }
+    //getter setter
+    
+    //addSpot
+    public void addSpot(ParkingSpot spot){
+        spots.put(spot.getSpotId(),spot);
+    }
+    
+    //findAvailableSpot
+    public ParkingSpot findAvailableSpot(Vehicle vehicle){
+        for(ParkingSpot s:spots.values()){
+                if(s.isAvailable() && s.canFitVehicle(vehicle)){
+                    return s;
+                }
+            }
+        return null;
+    }
+    
+    
+}
+
+//TICKET
+public class Ticket{
+    ticketId;
+    entryTime
+    exitTime
+    vehicle
+    SpotId
+    
+    //getDurationTime
+    
+}
